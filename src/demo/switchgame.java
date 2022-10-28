75be749d5dbb3b53d95b5b2e7b8a0328a5c62254
LMR <3120639307@qq.com>
2022-10-28 15:18:03 +0800

    完成任务

---
+++ src/demo/switchgame.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.Scanner;
+
+public class switchgame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("灯数");
+        int dengcount = input.nextInt();
+        System.out.println("人数");
+        int userCount = input.nextInt();
+    }
+}
