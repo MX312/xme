c26fa4a94ed5dee6827f4a4e1d98c3d2e367a1a0
LMR <3120639307@qq.com>
2022-10-28 14:39:32 +0800

    jinqiao

---
+++ src/demo/quyu.java
@@ -0,0 +1,30 @@
+package demo;
+
+import java.util.Scanner;
+
+public class quyu {
+    public static void main(String[] args) {
+        // 接收用户输入:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入整数n:");
+        int n = scanner.nextInt();
+
+        //开始遍历,来寻找最小的x,使用for循环做一个无限循环
+        int x=1;
+
+        //定义一个每次遍历的结果,来保存每次乘以2时的值,其实就是2的几次方2^x
+        int result=1;
+        for (; ; x++) {
+//            result=result*2; 传统写法,如果result和int类型不一致,有可能会报错
+            result*=2;  //简略写法,包含了隐式转化,这样的代码安全性比较高
+            if (result % n==1) {
+                System.out.println("2 ^"+x+" mod "+n+" = 1");
+                break;
+            }
+            if (x == n) {
+                System.out.println("2 ^? mod "+n+" = 1");
+            }
+
+        }
+    }
+}
